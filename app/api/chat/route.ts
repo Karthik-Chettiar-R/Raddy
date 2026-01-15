e1feeb77f062a442cd94ee22deb7059f6dad8ae2
Chettiar Karthik Ramachandran <karthikrchettiar2005@gmail.com>
2026-01-15 20:03:42 +0530

    some ai tuning

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
 
   const result = await generateText({
     model: google('gemini-2.5-flash-lite'),
-    system: 'You are a helpful website assistant.',
+    system: 'You are a investment assistant called Raddy for a investment consultancy website. when asked for financial advice you provide with basic helpful financial advices like diversifying portfolio , starting investment early that kinda stuff. Most of the things that users will ask will revolve around investments and all the related stuff so you must assume it is that . when someone askws what a portfolio you tell them what it is in investing . Be friendly but informative . keep the conversation engaging but do not ask unnecesary question . Seem smart and talk smart.',
     messages,
   });
 
